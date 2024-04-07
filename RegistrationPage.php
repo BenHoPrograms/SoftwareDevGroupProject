3f97784e639eb25d78ca40a7a6391eb35d564b00
Ominousss <ahmedabdalla7192@gmail.com>
2024-04-07 04:37:45 +0100

    Added registration page stylesheet and php file(only contains html atm)

---
+++ RegistrationPage.php
@@ -0,0 +1,55 @@
+<html>
+
+<head>
+    <title> CREATE an account </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="RegistrationPage.css">
+</head>
+
+<body>
+    
+<div class="container">
+    <form action="">
+        <h1>CREATE an account</h1>
+
+        <div class="box">
+            <div class="input-box">
+                <input type="text" placeholder="Email">
+                <i class='bx bxs-user'></i>
+            </div>
+
+            <div class="input-box">
+                <input type="text" placeholder="Password">
+                <i class='bx bxs-lock-alt' ></i>
+            </div>
+
+            <div class="input-box">
+                <input type="text" placeholder="Confirm Password">
+                <i class='bx bxs-lock' ></i>
+            </div>
+
+            <div class=license-terms>
+                <label>
+                    <input type="checkbox"> I agree to the license terms </input>
+                </label>
+            </div>
+
+            <button type="submit" class="btn">
+                SIGN UP
+            </button>
+
+            <div class="already-have-an-account">
+                <p>You already have an account?
+                    <a href="#">Login here</a>
+                </p>    
+            </div>
+        </div>
+
+    </form>
+</div>
+
+</body>
+
+</html>
