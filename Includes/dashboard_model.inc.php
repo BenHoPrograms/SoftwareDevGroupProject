aae2e326aac4537dbbe486ed65228585da7b8780
Ominousss <ahmedabdalla7192@gmail.com>
2024-04-15 01:21:55 +0100

    Fixed.

--- Includes/dashboard_model.inc.php
+++ Includes/dashboard_model.inc.php
@@ -4,7 +4,7 @@ function GetSimilarUsers(object $pdo, string $username)
 {
     $query = "SELECT username FROM profiles WHERE username LIKE :username;";
     $statement = $pdo->prepare($query);
-    $keyword_to_search = '%' . $username . '%';
+    $keyword_to_search = $username . '%';
     $statement->bindParam(':username', $keyword_to_search);
     $statement->execute();
 
