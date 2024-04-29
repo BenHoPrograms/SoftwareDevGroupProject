d9bd5de4f328f592920673a9f0fa19bb616dc0cf
Ominousss <ahmedabdalla7192@gmail.com>
2024-04-29 22:15:26 +0100

    Model methods didn't get added. Added them there

--- Includes/dashboard_model.inc.php
+++ Includes/dashboard_model.inc.php
@@ -17,3 +17,39 @@ function GetSimilarUsers(object $pdo, string $username)
         }
     }
 }
+
+function GetGenderUsers(object $pdo, string $gender) 
+{
+    $query = "SELECT username FROM profiles WHERE gender = :gender;";
+    $statement = $pdo->prepare($query);
+    $keyword_to_search = $gender;
+    $statement->bindParam(':gender', $keyword_to_search);
+    $statement->execute();
+
+    echo"<pre> You have searched for '".$gender. "' users:</pre>";
+    while ($r=$statement->fetch(PDO::FETCH_ASSOC)) 
+    {
+        foreach($r as $key => $value)
+        {
+            echo"<pre> User Found: ".$value."</pre>";
+        }
+    }
+}
+
+function GetAgeUsers(object $pdo, int $age) 
+{
+    $query = "SELECT username FROM profiles WHERE age = :age;";
+    $statement = $pdo->prepare($query);
+    $keyword_to_search = $age . '%';
+    $statement->bindParam(':age', $keyword_to_search);
+    $statement->execute();
+
+    echo"<pre> You have searched for users who are '".$age. "':</pre>";
+    while ($r=$statement->fetch(PDO::FETCH_ASSOC)) 
+    {
+        foreach($r as $key => $value)
+        {
+            echo"<pre> User Found: ".$value."</pre>";
+        }
+    }
+}
